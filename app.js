cc9b57842cd31a06fc9ecc48ab2442f5ee37ea86
Mike Yao <mikeyao@Mike-Yao.local>
2015-07-30 17:11:40 -0700

    Component based Vue prototyped

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
-Vue.component('coupon', {
-  template: '#coupon-template',
+Vue.component('projection', {
+  template: '#projection-template',
 
-  props: ['when-applied'],
+  // Vue automatically convert to Carmel-case as 'getProjection'
+  props: ['get-projection'],
 
   data: function(){
     return {
-      coupon: 'yaotime',
+      username: 'mikeyao',
       message: ''
     }
   },
 
   methods: {
-    whenCouponHasBeenEntered: function(){
+    whenUsernameHasBeenEntered: function(){
       this.validate();
     },
 
     validate: function(){
-      if (this.coupon === 'yaotime'){
-        console.log('Alert box!');
-        var response = this.whenApplied(this.coupon);
-        console.log('response: ' + response);
-
-        return this.message = '20% Off!';
+      if (this.username === 'mikeyao'){
+        // this.getProjection refer to props 'get-projection', which points to function 'getProjectionByUsername' in Vue parent instance
+        return this.message = 'Projection: ' + this.getProjection(this.username);
+        
       }
-      this.message = "This coupon is invalid!";
+      this.message = "This username is not invalid! 'mikeyao' is the only valid user! ";
     }
   }
 });
 
+
+Vue.component('projection-chart', {
+  template: 
+  "<div>" + 
+    "<h3>Projection: </h3>" +
+    "<h4 v-text='projection'></h4>" +
+    "<input type='text' v-model='projection'>" + 
+    "{{$data | json}}" +
+  "</div>",
+
+  data: function(){
+    return {
+      projection: '32,000,000'
+    };
+  }
+
+
+});
+
+
+
+
 new Vue({
-  el: '#scenario-chart',
+  el: '#scenarios',
 
   methods: {
-    setCoupon: function(coupon){
-      console.log('Set coupon:'+ coupon);
-      this.$set('coupon', coupon);
-
-      return 'thanks';
+    getProjectionByUsername: function(username){
+      this.$set('username', username);
+      var projection = 100; // Simulate API Call to get projection data
+      this.$set('projection', projection);
+      return this.projection;
     }
   }
 });
